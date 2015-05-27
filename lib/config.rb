58c4162c0420ed100c68396d8d9671582e31d200
party <6418006+conchyliculture@users.noreply.github.com>
2015-05-27 22:16:11 +0200

    remove ytdl cache

--- lib/config.rb
+++ lib/config.rb
@@ -2,4 +2,5 @@ CONFIG={
     :dsn => "DBI:SQLite3:#{File.join(File.dirname(__FILE__),"..",'party.sqlite')}",
     :ytdlcmd => "youtube-dl",
     :ytdldestdir => "/home/fete/www/party/public/videos/",
+    :extraytdlargs => "--no-cache-dir",
 }
