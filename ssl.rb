f7e841ddd762931bcb883f534e0756d0f341d52e
fete <github@renzokuken.eu>
2015-05-30 13:02:14 +0200

    move ssl config to config.rb

--- ssl.rb
+++ ssl.rb
@@ -1,20 +1,24 @@
+$: << File.join(File.dirname(__FILE__),'lib')
+
 require 'webrick/https'
+
+require "config.rb"
 
 # Thx to http://stackoverflow.com/a/8952137
 module Sinatra
   class Application
     def self.run!
-      certificate_content = File.open(ssl_certificate).read
-      key_content = File.open(ssl_key).read
+      certificate_content = File.open(CONFIG[:ssl_server_certificate]).read
+      key_content = File.open(CONFIG[:ssl_server_key]).read
 
       server_options = {
-        :Host => ssl_host,
-        :Port => ssl_port,
+        :Host => CONFIG[:ssl_host],
+        :Port => CONFIG[:ssl_port],
         :SSLEnable => true,
         :SSLCertificate => OpenSSL::X509::Certificate.new(certificate_content),
         :SSLPrivateKey => OpenSSL::PKey::RSA.new(key_content),
         :SSLVerifyClient => OpenSSL::SSL::VERIFY_PEER|OpenSSL::SSL::VERIFY_FAIL_IF_NO_PEER_CERT,
-        :SSLCACertificateFile => ssl_ca, 
+        :SSLCACertificateFile => CONFIG[:ssl_CA_certificate], 
       }
 
       Rack::Handler::WEBrick.run self, server_options do |server|
