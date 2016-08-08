83655e6cb958c7f5d1e37008180f4606daf6d96b
party <6418006+conchyliculture@users.noreply.github.com>
2016-08-08 15:15:44 +0200

    works

--- lib/db.rb
+++ lib/db.rb
@@ -6,6 +6,7 @@ class PartyDB
     if not Object.const_defined?(:DB)
         DB = Sequel.sqlite 'party.sqlite', :loggers => [Logger.new($stderr)]
     end
+    Sequel::Model.plugin :force_encoding, 'UTF-8'
     Sequel::Model.plugin(:schema)
 
     class Infos < Sequel::Model(:infos)
@@ -26,7 +27,7 @@ class PartyDB
     end
 
     def PartyDB.add_video(infos)
-        Infos.insert("INSERT INTO infos  VALUES (null,?,?,?,?,?,?)",
+        Infos.insert(
                 :yid =>infos[:yid],
                 :title => infos[:title],
                 :file => infos[:file],
@@ -39,78 +40,32 @@ class PartyDB
     def PartyDB.get_rand(count=10)
         res=[]
         Infos.order(Sequel.lit('RANDOM()')).limit(count).each{|rs|  res << rs.to_hash}
-#        @dbh.select_all("SELECT id,yid,title,file,description,comment,url FROM infos ORDER BY RANDOM() LIMIT ?",count).each do |row|
-#            res << {
-#                :id => row[0],
-#                :yid => row[1],
-#                :title => row[2],
-#                :file => row[3],
-#                :description => row[4],
-#                :comment => row[5],
-#                :url => row[6],
-#            }
-#        end
         res
     end
 
     def PartyDB.get_from_id(id)
-        Infos.where(:id => id).to_hash
-#        row=@dbh.select_one("SELECT id,yid,title,file,description,comment,url FROM infos WHERE id = ?",id)
-#        res = {
-#            :id => row[0],
-#            :yid => row[1],
-#            :title => row[2],
-#            :file => row[3],
-#            :description => row[4],
-#            :comment => row[5],
-#            :url => row[6],
-#        }
-#        res
+        Infos.where(:id => id).first.to_hash
     end
 
-    def PartyDB.search(q)
-        res=[]
-        Infos.where{(Sequel.like(:title,  q)) | (Sequel.like(:comment,  q))}.each {|rs| res << rs.to_hash}
-#        @dbh.select_all("SELECT id,yid,title,file,description,comment,url FROM infos WHERE title LIKE ? or comment LIKE ?","%#{q}%","%#{q}%").each do |row|
-#            res << {
-#                :id => row[0],
-#                :yid => row[1],
-#                :title => row[2],
-#                :file => row[3],
-#                :description => row[4],
-#                :comment => row[5],
-#                :url => row[5],
-#            }
-#        end
-#        res
+    def PartyDB.search(qq)
+        q="%#{qq}%"
+        Infos.where{(Sequel.ilike(:title,  q)) | (Sequel.ilike(:comment,  q))}
     end
 
     def PartyDB.get_file_from_id(id)
         return "lol?" unless id=~/^\d+$/
-#        res=@dbh.select_one("SELECT file FROM infos WHERE id = ?",id)
-        Infos.select(:file).where(:id => id).first
-        return res
+        Infos.select(:file).where(:id => id).first[:file]
     end
 
     def PartyDB.already_in_db?(url)
-        #res=@dbh.select_one("SELECT id FROM infos WHERE url=?",url)
-        return ! Infos.where(:url=>url).empty?
-        #return res != nil
+        if Infos.grep(:url,"%#{url}%").empty?
+            return !Infos.where(:yid=>url).empty?
+        end
+        return true
     end
 
     def PartyDB.set_comment(id,t)
-        #@dbh.do("UPDATE infos SET comment=? WHERE id =?",t,id)
         Infos.where(:id => id).update(:comment => t)
     end
-
-
-#    def PartyDB.import_from_ytfacts(dsn)
-#        dbh = Sequel.connect("mysql://ytdluser:ytdlpass@10.0.3.10/youtube_dl")
-#        dbh.fetch("SELECT yid, description, title,  file FROM infos WHERE bien = 1") do  |row|
-#            tags=dbh.fetch("SELECT tag_name FROM tags WHERE yid  =? ",row[:yid])
-#            @dbh["INSERT INTO infos (id,yid,title,description,comment) VALUES (NULL,?,?,description,NULL)",row[:yid],row[:title],row[:description]]
-#        end
-#
-#    end
 
 end
